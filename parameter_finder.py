b7508b82e3197a9c73b199bc34aa981317c6ed45
Austin Jones <austin.jones@uster.com>
2024-11-04 11:40:19 -0500

    idk

--- parameter_finder.py
+++ parameter_finder.py
@@ -10,16 +10,26 @@ def finder(hist, desired, f):
     scale = desired / actual
 
     for i in range(10):
-        new_hist = hist.stretch_into(abs(scale))
+        new_hist = hist.stretch_into(scale)
         new_actual = f(new_hist)
-        scale += (desired - new_actual) / desired
-        print(scale,desired, new_actual, len(new_hist.hist))
+        scale += 1 - (desired / new_actual)
+        print(scale,desired, new_actual)
 
     print()
     return scale
 
 def thing(hist, w = 1):
-    return sum(v * i * w for i, v in enumerate(hist.hist)) / sum(v for v in hist.hist)
+    return sum(v * (( i * w ) + (w / 2)) for i, v in enumerate(hist.hist)) / sum(v for v in hist.hist)
+
+
+def make_test_hist():
+    s = "AAAAAAAAAAAMAAAAYgAAAKEAAACYAAAAhQAAAHoAAACPAAAAeAAAAGcAAABrAAAAcwAAAGoAAABLAAAAXwAAAEQAAABPAAAAXgAAAEsAAABRAAAARQAAAEgAAABPAAAAUQAAAEEAAABIAAAARwAAAEMAAABHAAAANgAAADUAAAA="
+
+    a = base64.b64decode(s)
+
+    a_int = bytes_to_arr(a, 4) 
+
+    return Histogram.from_array(a_int)
 
 
 if __name__ == '__main__':
@@ -27,20 +37,13 @@ if __name__ == '__main__':
     # current_time_seconds = time.time()
     seed(0)
 
-    s = "AAAAAAAAAAAMAAAAYgAAAKEAAACYAAAAhQAAAHoAAACPAAAAeAAAAGcAAABrAAAAcwAAAGoAAABLAAAAXwAAAEQAAABPAAAAXgAAAEsAAABRAAAARQAAAEgAAABPAAAAUQAAAEEAAABIAAAARwAAAEMAAABHAAAANgAAADUAAAA="
-
-    a = base64.b64decode(s)
-
-    a_int = bytes_to_arr(a, 4) 
-
-    hist = Histogram.from_array(a_int)
+    hist = make_test_hist()
 
     # hist.print(100_000_000)
     # print()
 
-    f = lambda h : thing(h, 3)
+    f = lambda h : thing(h, 2)
 
-    print("initial:", len(hist.hist))
     print("initial:", f(hist))
     print()
 
