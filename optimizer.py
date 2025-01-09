98931757653356d5e4cee654f778eed0da97ab8c
Austin Jones <austin.jones@uster.com>
2025-01-09 11:24:18 -0500

    simplify

--- optimizer.py
+++ optimizer.py
@@ -1,95 +1,96 @@
 from random import random, seed
 from power_set import PowerSet
 
+def sim_anneal(iteration, max_iters):
+    # weight temp the based on energy
+    t_max = 2 * best_error
+    t = t_max * (1 - (iteration / max_iters))
+
+    energy = current_error - best_error
+
+    p = (energy + t) / (best_error + t_max)
+    r = random()
+
+    return p >= r
+
 def custom_optimizer(
         calculate_error,
-        initial_m=1,
-        initial_b=0,
+        initial,
         step_size=1,
         search_width=1,
         max_iters=100
     ):
     # Initialize coefficients and other parameters
-    m, b = initial_m, initial_b
+    params = initial
     best_error = float('inf')
-    best_m, best_b = m, b
+    best_params = params
+
+    # pregenerate our delta arrays
+    steps = [i * step_size for i in range(-search_width, search_width + 1)][::-1]
+    ps = PowerSet(2, len(steps))
+    deltas = [
+        (
+            [steps[ps.set[idx]] for idx in range(len(params))],
+            ps.inc()
+        )[0]
+        for _ in range(ps.total_iteration())
+    ]
     
     # Optimization loop
     for iteration in range(max_iters):
         # Calculate error with current m and b
-        current_error = calculate_error(m, b)
+        current_error = calculate_error(params)
 
         print(iteration)
-        print(best_m, best_b, best_error, step_size)
+        print(params, best_error, step_size)
         
         # Check if current error is the best we've seen
-        if current_error < best_error:
+        if current_error < best_error or sim_anneal(iteration, max_iters):
             best_error = current_error
-            best_m, best_b = m, b
-        else:
-            # weight temp the based on energy
-            t_max = 2 * best_error
-            t = t_max * (1 - (iteration / max_iters))
+            best_params = params
 
-            energy = current_error - best_error
+            if current_error >= best_error:
+                step_size *= 0.95
 
-            p = (energy + t) / (best_error + t_max)
-            r = random()
+        for delta in deltas:
 
-            print(">", p, r)
-            print()
 
-            if p >= r:
-                best_error = current_error
-                best_m, best_b = m, b
+            new_params = [params[i] + delta[i] for i in range(len(params))]
 
-            step_size *= 0.95
-
-        # Try adjusting 'm' and 'b' in both positive and negative directions
-        steps = [i * step_size for i in range(-search_width, search_width + 1)][::-1]
-        ps = PowerSet(2, len(steps))
-
-        for _ in range(ps.total_iteration()):
-            delta_m = steps[ps.set[0]]
-            delta_b = steps[ps.set[1]]
-            ps.inc()
-
-            if delta_m == delta_b == 0:
-                continue
-            # Calculate new potential values for m and b
-            new_m, new_b = m + delta_m, b + delta_b
-
-            new_error = calculate_error(new_m, new_b)
+            new_error = calculate_error(new_params)
             
             # Update m and b if new error is better
             if new_error < current_error:
-                m, b = new_m, new_b
+                params = new_params
                 current_error = new_error
 
         if abs(step_size) < 1e-6 or best_error < 1e-6:
             break
     
-    return best_m, best_b
+    return best_params
     
 def test_basic_optimizer():
     f = lambda x: (x - 3) * (x - 1)
 
-    def err(x0, x1):
-
+    def err(arr):
+        x0 = arr[0]
+        x1 = arr[1]
         y0 = f(x0)
         y1 = f(x1)
 
         return abs(y0) + abs(y1)
 
-    x, y = custom_optimizer(
+    res = custom_optimizer(
         err,
-        initial_m=0,
-        initial_b=10,
+        initial=[0, 10],
         step_size=0.5,
         max_iters=1000
     )
+    x = res[0]
+    y = res[1]
 
     print()
+    print(res)
     print(x, y)
     print(f(x))
     print(f(y))
